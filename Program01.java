f418b75ba0e64ae0007e89e0c7b208af0fbbba0d
vijaykumarr6281 <129029216+vijaykumarr6281@users.noreply.github.com>
2023-03-27 10:40:22 +0530

    Add files via upload

---
+++ Program01.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class program01
+{
+public static void main(String args[])
+{
+Scanner scob=new Scanner(System.in);
+int num=scob.nextInt();
+if (num/2*2==num)System.out.println("EVEN");
+if (num/2*2!=num)System.out.println("ODD");
+}//main()
+}//Program01
